88f2a82116a9c4be66cfd4b5f4ac29f85c182d16
Thrillz <kingthrillz@gmail.com>
2017-08-17 09:59:32 +0100

    angular intro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { User } from './shared/models/user';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  message: string = "Hello!";
+  users: User[] = [
+    {id: 10, name: "Uche", username: "Dad-Vader"},
+    {id: 11, name: "Dami", username: "Damiboy"},
+    {id: 12, name: "Tboy", username: "Obi-million"}
+  ];
+  activeUser: User;
+
+  selectUser(user) {
+    this.activeUser = user;
+    console.log(this.activeUser);
+  }
+
+  onUserCreated(event) {
+    console.log(event);
+    this.users.push(event.user);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { UserProfileComponent } from './users/user-profile.component';
+import { UserFormComponent } from './users/user-form.component';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserProfileComponent,
+    UserFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
